c56e16f10b12a471512714db0e33c0f6c2f0c866
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-05-15 09:25:56 -0300

    Remoção de imports desnecessários em Forma

--- src/turbine/Forma.java
+++ src/turbine/Forma.java
@@ -1,29 +1,7 @@
 package turbine;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
-
-import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
-import com.jogamp.opengl.GLProfile;
-import com.jogamp.opengl.glu.GLU;
-import com.jogamp.opengl.glu.GLUquadric;
-import com.jogamp.opengl.util.gl2.GLUT;
-import com.jogamp.opengl.util.texture.Texture;
-import com.jogamp.opengl.util.texture.TextureData;
-import com.jogamp.opengl.util.texture.TextureIO;
+
 
 public abstract class Forma {
     public abstract void desenha(GL2 gl2);
