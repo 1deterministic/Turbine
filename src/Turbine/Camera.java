26b9f683cca346860b95ec399ee277a3d13412f0
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-06-01 13:11:17 -0300

    Transições desabilitadas temporariamente

--- src/Turbine/Camera.java
+++ src/Turbine/Camera.java
@@ -5,13 +5,14 @@ import com.jogamp.opengl.GL2;
 public class Camera {
     // atributos temporariamente públicos
     public Ponto local; // local da câmera no espaço
-    public Ponto rotacao; // rotação na câmera, por ângulo nos 3 eixos. Talvez mude para um vetor de rotação e um ângulo
+    //public Ponto rotacao; // rotação na câmera, por ângulo nos 3 eixos. Talvez mude para um vetor de rotação e um ângulo
+    public Ponto direcao;
     public Objeto anexo; // objeto anexado a essa câmera, de modo que ela o siga
     
     // construtor padrão, cria a câmera na posição 0, 0, 0
     public Camera() {
-        this.local = new Ponto(0d, 0d, 0d);
-        this.rotacao = new Ponto(0d, 0d, 0d);
+        this.local = new Ponto();
+        this.direcao = new Ponto();
     }
     
     // caso seja preciso alterar o local de uma câmera sem que ela esteja anexada a um objeto
@@ -34,7 +35,7 @@ public class Camera {
         // Inicializa sistema de coordenadas do modelo
         ogl.gl.glLoadIdentity();
         // Especifica posição do observador e do alvo
-        ogl.glu.gluLookAt(this.local.x, this.local.y, this.local.z, this.anexo.getLocal().x, this.anexo.getLocal().y, this.anexo.getLocal().z, 0, 1, 0);
+        ogl.glu.gluLookAt(this.local.x, this.local.y, this.local.z, this.direcao.x, this.direcao.y, this.direcao.z, 0, 1, 0);
         //ogl.gl.glTranslated(-this.local.x, -this.local.y, -this.local.z);
         //ogl.gl.glRotated(this.rotacao.x, 1, 0, 0);
         //ogl.gl.glRotated(this.rotacao.y, 0, 1, 0);
@@ -49,18 +50,30 @@ public class Camera {
     
     // transiciona essa câmera para o local indicado pelo objeto que ela deve seguir
     public void transicaoCamera(Double deltaTime) {
-//         a câmera ficará travada no objeto caso a distância seja menor do que a distância percorrida pelo objeto em 2 frames + 50cm (no caso de o objeto estar parado)
-        if (this.local.getDistancia(anexo.getLocalCamera()) < 2 * this.anexo.getVelocidade() * deltaTime + 0.5d) {
-            this.local = anexo.getLocalCamera();
-       // caso a dustância seja maior, a câmera se aproximará do objeto a uma velocidade igual à soma da velocidade do objeto mais a distância entre eles
-       // dessa forma ela se aproximará mais rápido se estiver mais distante e mais devagar quando estiver perto
-       // em uma velocidade que é sempre maior do que a velocidade do objeto que ela irá seguir
-        } else {
-            Double velocidade = this.anexo.getVelocidade() + 2d * this.local.getDistancia(this.anexo.getLocal());
-            
-            // calcula a direção na qual a câmera precisa se movimentar
-            Ponto vetorDirecao = new Ponto(this.anexo.getLocalCamera().x - this.local.x, this.anexo.getLocalCamera().y - this.local.y, this.anexo.getLocalCamera().z - this.local.z).versor();
-            this.transladar(vetorDirecao.versor().escalar(velocidade * deltaTime));
-        }
+        this.local = anexo.getLocalCamera();
+        this.direcao = this.anexo.getLocal();
+//        
+////         a câmera ficará travada no objeto caso a distância seja menor do que a distância percorrida pelo objeto em 2 frames + 50cm (no caso de o objeto estar parado)
+//        if (this.local.getDistancia(anexo.getLocalCamera()) < 2 * this.anexo.getVelocidade() * deltaTime + 0.5d) {
+//            this.local = anexo.getLocalCamera();
+//       // caso a dustância seja maior, a câmera se aproximará do objeto a uma velocidade igual à soma da velocidade do objeto mais a distância entre eles
+//       // dessa forma ela se aproximará mais rápido se estiver mais distante e mais devagar quando estiver perto
+//       // em uma velocidade que é sempre maior do que a velocidade do objeto que ela irá seguir
+//        } else {
+//            Double velocidade = this.anexo.getVelocidade() + 2d * this.local.getDistancia(this.anexo.getLocal());
+//            
+//            
+//            // calcula a direção na qual a câmera precisa se movimentar
+//            Ponto vetorDirecao = new Ponto(this.anexo.getLocalCamera().x - this.local.x, this.anexo.getLocalCamera().y - this.local.y, this.anexo.getLocalCamera().z - this.local.z);
+//            this.transladar(vetorDirecao.versor().escalar(velocidade * deltaTime));
+//        }
+//        
+//        if (this.direcao.getDistancia(this.anexo.getLocal()) < 3d) {
+//            this.direcao = this.anexo.getLocal();
+//        } else {
+//            Double velocidade = this.direcao.getDistancia(this.anexo.getLocal()) * 2d + 0.1d;
+//            Ponto vetorDirecao = new Ponto(this.anexo.getLocal().x - this.direcao.x, this.anexo.getLocal().y - this.direcao.y, this.anexo.getLocal().z - this.direcao.z);
+//            this.direcao.somar(vetorDirecao.escalar(deltaTime));
+//        }
     }
 }
